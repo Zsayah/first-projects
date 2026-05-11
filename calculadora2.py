b6ff15a0347748b05bd09274b3d52935ec10c468
Zsayah <gabrielescaleira3848@gmail.com>
2026-05-11 18:06:03 -0300

    primeiro commit

---
+++ calculadora2.py
@@ -0,0 +1,48 @@
+def somar(a, b):
+    return a + b
+def subtrair(a, b):
+    return a - b
+def multiplicar(a, b):
+    return a * b
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return 'Erro'
+
+
+while True:
+    print('===Calculadora===')
+
+    n1 = float(input(' Digite um Numero:'))
+    n2 = float(input('Digite outro Numero:'))
+
+    print('Escolha um simbolo dentre as opçoes +, -, /, *')
+
+    simbolo = input('Digite um Simbolo: ')
+
+    if simbolo == '+':
+        resultado = somar(n1, n2)
+
+    elif simbolo == '-':
+        resultado = subtrair(n1, n2)
+
+    elif simbolo == '*':
+        resultado = dividir(n1, n2)
+
+    elif simbolo == '/':
+        resultado = dividir(n1, n2)
+    else:
+        resultado = 'Símbolo Inválido'
+
+    print(f'Resultado = {resultado}')
+
+    continuar = input('Gostaria de continuar? (s/n):  ')
+    if continuar.lower() != 's':
+        print('Terminando a calculadora!')
+        break
+
+
+
+
+
